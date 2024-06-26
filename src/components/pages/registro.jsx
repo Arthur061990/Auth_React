cc8ab18b2c8542b7daf9a57acf03805f7511aef8
Arthur061990 <artdlacueva@gmail.com>
2024-06-26 14:08:13 -0600

    estilo registro css

--- src/components/pages/registro.jsx
+++ src/components/pages/registro.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import { useRef } from 'react';
+import '../Estilos/estilo.css'; 
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 import axios from 'axios';
 
@@ -30,32 +32,35 @@ export default function Registro() {
     }
 
     return (
-        <Form ref={formRef} onSubmit={handleSubmit}>
-        <Form.Group className="mb-3" controlId="formBasicEmail">
-          <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Ingresa Correo Electronico" name="email" /> 
-          <Form.Text className="text-muted">
-            Nunca mostraremos tu correo con nadie
-          </Form.Text>
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="formBasicEmail">
-          <Form.Label>Username</Form.Label>
-          <Form.Control type="text" placeholder="Ingresa Username" name="username" />
-          <Form.Text className="text-muted">
-            Nunca mostraremos tu username
-          </Form.Text>
-        </Form.Group>
-  
-        <Form.Group className="mb-3" controlId="formBasicPassword">
-          <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" name="password"/>
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="formBasicCheckbox">
-          <Form.Check type="checkbox" label="Check me out" />
-        </Form.Group>
-        <Button variant="primary" type="submit">
-          Submit
-        </Button>
-      </Form>
+      <div className="form-container-wrapper">
+      <div className="form-container">
+          <Form ref={formRef} onSubmit={handleSubmit}>
+              <Form.Group className="mb-3" controlId="formBasicEmail">
+                  <Form.Label>INGRESA TU CORREO ELECTRONICO</Form.Label>
+                  <Form.Control type="email" name="email" />
+                  <Form.Text className="text-muted">
+                      Nunca mostraremos tu correo con nadie
+                  </Form.Text>
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="formBasicUsername">
+                  <Form.Label>INGRESA TU NOMBRE DE USUARIO</Form.Label>
+                  <Form.Control type="text" name="username" />
+                  <Form.Text className="text-muted">
+                      Nunca mostraremos tu username
+                  </Form.Text>
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="formBasicPassword">
+                  <Form.Label>INGRESA LA CONTRASEÑA</Form.Label>
+                  <Form.Control type="password" name="password" />
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                  <Form.Check type="checkbox" label="ACEPTO LOS TERMINOS Y CONDICIONES" />
+              </Form.Group>
+              <Button variant="primary" type="submit">
+                  REGISTRARME
+              </Button>
+          </Form>
+      </div>
+  </div>
     )
 }
